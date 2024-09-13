72ea3979c7f43f047def3bde9ede828a497571f0
adrioviedo28 <adrioviedov@gmail.com>
2024-09-13 11:37:23 -0500

    cambio caso 7

--- src/Menu.java
+++ src/Menu.java
@@ -191,8 +191,84 @@ public class Menu {
                     break;
 
                 case 7:
-                    System.out.println("Imprime el menu Condicional ternaria");
-                    break;
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("                       TERNARIA                        ");
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("|     Elige la opcion que deseas ver                |");
+                    System.out.println("|     1. Explicación de Ternaria                    |");
+                    System.out.println("|     2.Programa de Ternaria -Determina si un número|");
+                    System.out.println("|     es Par o Impar -Digite una opción             |");
+                    System.out.println("-----------------------------------------------------");
+
+                    System.out.println("Selecciona una opcion");
+                    Scanner Tternaria = new Scanner(System.in);
+                    int opcionTernaria = Tternaria.nextInt();
+
+                    if (opcionTernaria == 1) {
+                        System.out.println("----------------------------------------------------------------------------------");
+                        System.out.println("                             Explicación de Ternaria                              ");
+                        System.out.println("----------------------------------------------------------------------------------");
+                        System.out.println("| la condicional ternaria en Java es una forma compacta de escribir              |");
+                        System.out.println("| una instrucción if-else. Te permite decidir entre dos valores basados          |");
+                        System.out.println("| en una condición, todo en una sola línea.                                      |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("| La sintaxis básica es:                                                         |");
+                        System.out.println("| resultado = (condición) ? valor_si_verdadero : valor_si_falso;                 |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("| condición:                                                                     |");
+                        System.out.println("| Es una expresión que se evalúa como true o false.                              |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("| valor_si_verdadero:                                                            |");
+                        System.out.println("| Es el valor que se asigna a la variable resultado si la condición es true.     |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("| valor_si_falso:                                                                |");
+                        System.out.println("| Es el valor que se asigna a la variable resultado si la condición es false.    |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("|  Ejemplo:                                                                      |");
+                        System.out.println("| verificar si un número entero es par o impar y mostrar un mensaje              |");
+                        System.out.println("| en función de eso                                                              |");
+                        System.out.println("| public class ParImpar {                                                        |");
+                        System.out.println("| public static void main(String[] args) {                                       |");
+                        System.out.println("| int numero = 7; // Puedes cambiar este número para probar con diferentes       |");
+                        System.out.println("| valores                                                                        |");
+                        System.out.println("| // Usando condicional ternaria para determinar si el número es par o impar     |");
+                        System.out.println("| String resultado = (numero % 2 == 0) ? \"El número es par\" : \"El número es impar\";|");
+                        System.out.println("| // Mostrar el resultado                                                        |");
+                        System.out.println("| System.out.println(resultado);                                                 |");
+                        System.out.println("|     }                                                                          |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("|  numero % 2 == 0 :Esta es la condición que verificamos. El operador % calcula  |");
+                        System.out.println("|  el residuo de la división del numero entre 2. Si el residuo es 0, entonces    |");
+                        System.out.println("|  el número es par.                                                             |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("| \"El número es par\": Este es el valor asignado a resultado si la condición es |");
+                        System.out.println("| verdadera (es decir, si el número es par).                                     |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("| \"El número es impar\": Este es el valor asignado a resultado si la condición  |");
+                        System.out.println("|  es falsa (es decir, si el número es impar).                                   |");
+                        System.out.println("|                                                                                |");
+                        System.out.println("| En el ejemplo, el número 7 es impar, por lo que el resultado será              |");
+                        System.out.println("| \"El número es impar\".                                                        |");
+                        System.out.println("----------------------------------------------------------------------------------");
+                        System.out.println("Presiona Enter para continuar...");
+                        scannerMenuPrincipal.nextLine(); // Captura el salto de línea restante
+                        scannerMenuPrincipal.nextLine(); // Espera a que el usuario presione Enter
+                        break;
+
+                    } else if (opcionTernaria == 2) {
+                        Scanner numeroIngresado = new Scanner(System.in);
+                        System.out.println("Digite un número Entero");
+                        int numero = numeroIngresado.nextInt();
+
+                        String resultado = (numero % 2 == 0) ? "El número es par" : "El número es impar";
+                        // Mostrar el resultado
+                        System.out.println(resultado);
+                        System.out.println("Presiona Enter para continuar...");
+                        scannerMenuPrincipal.nextLine(); // Captura el salto de línea restante
+                        scannerMenuPrincipal.nextLine(); // Espera a que el usuario presione Enter
+                        break;
+
+                    }
 
                 case 8:
                     System.out.println("Imprime el menu Bucle DO WHILE");
@@ -206,14 +282,16 @@ public class Menu {
                     System.out.println("Imprime el menu Bucle FOR");
                     break;
 
+               }
 
 
-
-
-            }
         }while (opciones != 0);
         System.out.println("Saliendo del sistema...");
 
     }
 }
+
+
+
+
 
