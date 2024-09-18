56caf25b8238b31ee589eef6fc69a67809fdac8c
alejamesias <alejaam234@gmail.com>
2024-09-18 12:01:41 -0500

    Organizacion de clases y codigo

--- src/explicaciones/Detalle.java
+++ src/explicaciones/Detalle.java
@@ -33,4 +33,177 @@ public class Detalle {
         System.out.println("------------------------------------------------------------------");
         System.out.println("Presiona Enter para continuar...");
     }
+    public static void intExplicacion(){
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("                 ¿Que es un Int?                             ");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("| Es un tipo de dato primitivo que almacena números enteros |");
+        System.out.println("| y es el mas usado comunmente.                             |");
+        System.out.println("| Su rango es de: -2,147,483,648 a 2,147,483,647.           |");
+        System.out.println("| Sintaxis:                                                 |");
+        System.out.println("| int edad = 25;                                            |");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+
+    }
+    public static void longExplicacion(){
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("                 ¿Que es un Long?                            ");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("| Es un tipo de dato primitivo que almacena números enteros |");
+        System.out.println("|  de mayor tamaño. Se usa para almacenar numeros enteros   |");
+        System.out.println("| grandes que exceden el rango de tipo INT                  |");
+        System.out.println("| Su rango de valores es de:                                |");
+        System.out.println("| -9,223,372,036,854,775,808 a 9,223,372,036,854,775,807.   |");
+        System.out.println("| Sintaxis:                                                 |");
+        System.out.println("| long distanciaALaLuna = 384400000L;                       |");
+        System.out.println("| // Especificar 'L' al final para denotar un valor long    |");
+        System.out.println("-------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void floatExplicacion(){
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("                 ¿Que es un Float?                                 ");
+        System.out.println("------------------------------------------------------------------ ");
+        System.out.println("| se utiliza para almacenar números decimales (en punto flotante) |");
+        System.out.println("| de precisión simple.                                            |");
+        System.out.println("| Es adecuado  para cálculos que no requieren alta precisión      |");
+        System.out.println("| decimal, como en gráficos y cálculos científicos.               |");
+        System.out.println("| Sintaxis:                                                       |");
+        System.out.println("| float temperatura = 36.6f;                                      |");
+        System.out.println("| float pi = 3.14159f;                                            |");
+        System.out.println("| // Especificar 'f' al final para denotar un valor float         |");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void doubleExplicacion(){
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("                 ¿Que es un Double?                                ");
+        System.out.println("------------------------------------------------------------------ ");
+        System.out.println("| se utiliza para almacenar números decimales (en punto flotante) |");
+        System.out.println("| de precisión doble.                                             |");
+        System.out.println("| Es adecuado  para cálculos que requieren de mayor precisión     |");
+        System.out.println("| decimal, como en cálculos científicos, financieros, etc.        |");
+        System.out.println("| Sintaxis:                                                       |");
+        System.out.println("| double areaDeLaTierra = 510.1E6;                                |");
+        System.out.println("| double velocidadDeLuz = 299792458.0;                            |");
+        System.out.println("| // Especificar 'd' es opcional, ya que 'double'                 |");
+        System.out.println("| es el tipo por defecto                                          |");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void charExplicacion(){
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("                 ¿Que es un Char?                                     ");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("| Es un tipo de dato primitivo que almacena un solo carácter Unicode.|");
+        System.out.println("| Se suele usar para almacenar y manipular caracteres individuales,  |");
+        System.out.println("| como letras, números, o símbolos.                                  |");
+        System.out.println("| Sintaxis:                                                          |");
+        System.out.println("| char letra = 'A';                                                  |");
+        System.out.println("| char simbolo = '#';                                                |");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void booleanExplicacion(){
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("                 ¿Que es un Boolean?                                  ");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("| Es un tipo de dato primitivo que almacena un valor booleano.       |");
+        System.out.println("| Puede tener solo dos valores: true o false.                        |");
+        System.out.println("| Se usa comunmente para representar estados lógicos o de control,   |");
+        System.out.println("| como en condicionales y bucles.                                    |");
+        System.out.println("| Sintaxis:                                                          |");
+        System.out.println("| boolean esMayorDeEdad = true;                                      |");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void operadoresAritmeticos(){
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("                 ¿Que son los Operadores Aritmeticos?                            ");
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("| Estos operadores se usan para realizar cálculos matemáticos.                   |");
+        System.out.println("| Suma (+): int a = 5 + 3; // a es 8                                             |");
+        System.out.println("| Resta (-): int b = 10 - 4; // b es 6                                           |");
+        System.out.println("| Multiplicacion (*): int c = 7 * 2; // c es 14                                  |");
+        System.out.println("| Division (/): int d = 20 / 4; // d es 5                                        |");
+        System.out.println("| Modulo int e = 10 % 3; // e es 1                                               |");
+        System.out.println("| el modulo devuelve el residuo de la division                                   |");
+        System.out.println("----------------------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void operadoresRelacionales(){
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("                      ¿Que son los Operadores Relacionales?                          ");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("| Estos operadores comparan dos valores y devuelven un valor booleano (true o false)|");
+        System.out.println("| Igual a (==): boolean resultado = (5 == 5); // resultado es true                  |");
+        System.out.println("| Diferente de (!=): boolean resultado = (5 != 3); // resultado es true             |");
+        System.out.println("| Mayor que (>): boolean resultado = (7 > 4); // resultado es true                  |");
+        System.out.println("| Menor que (<): boolean resultado = (3 < 6); // resultado es true                  |");
+        System.out.println("| Mayor o igual que (>=): boolean resultado = (5 >= 5); // resultado es true        |");
+        System.out.println("| Menor o igual que (<=): boolean resultado = (4 <= 7); // resultado es true        |");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void operadoresLogicos(){
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("                      ¿Que son los Operadores Logicos?                               ");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("| Estos operadores se usan para combinar condiciones booleanas.                     |");
+        System.out.println("| Y logico (&&): Devuelve true si ambas condiciones son verdaderas.                 |");
+        System.out.println("| » boolean resultado = (5 > 3 && 8 > 6); // resultado es true                      |");
+        System.out.println("| O logico (||): Devuelve true si al menos una de las condiciones es verdadera      |");
+        System.out.println("| » boolean resultado = (5 > 3 || 8 < 6); // resultado es true                      |");
+        System.out.println("| No logico (!): Devuelve el valor opuesto de la condicion                          |");
+        System.out.println("| » boolean resultado = !(5 > 3); // resultado es false                             |");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void operadoresUnarios(){
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("                      ¿Que son los Operadores Unarios?                               ");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("| Estos operadores actúan sobre un solo valor.                                      |");
+        System.out.println("| Incremento (++): Aumenta el valor de uno en uno                                   |");
+        System.out.println("| » int a = 5; a++; // a es 6                                                       |");
+        System.out.println("| Decremento(--): Disminuye el valor de uno en uno                                  |");
+        System.out.println("| » int b = 5; b--; // b es 4                                                       |");
+        System.out.println("| Negacion (-): Cambia el signo de un valor                                         |");
+        System.out.println("| » int c = 5; int d = -c; // d es -5                                               |");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void operadoresAsignacion(){
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("                      ¿Que son los Operadores de Asignacion?                          ");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("| Estos operadores asignan valores a variables.                                      |");
+        System.out.println("| Asignacion Simple (=): Asigna valor a una variable                                 |");
+        System.out.println("| » int x = 10; // x es 10                                                           |");
+        System.out.println("| Asignacion suma (+=): Suma y asigna el resultado                                   |");
+        System.out.println("| » int y = 5; y += 3; // y es 8                                                     |");
+        System.out.println("| Asignacion Resta (-=): Resta y asigna el resultado                                 |");
+        System.out.println("| » int z = 10; z -= 4; // z es 6                                                    |");
+        System.out.println("| y asi con todas las operaciones matematicas basicas                                |");
+        System.out.println("---------------------------------- ---------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void operadoresTernarios(){
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("                      ¿Que son los Operadores de Ternarios?                           ");
+        System.out.println("--------------------------------------------------------------------------------------");
+        System.out.println("| Es una forma compacta de realizar una operación condicional.                       |");
+        System.out.println("| Operador Ternario (?): Elige una opcion basado en una condicion                    |");
+        System.out.println("| » int edad = 18;                                                                   |");
+        System.out.println("| String resultado = (edad >= 18) ? Adulto : Menor                                   |");
+        System.out.println("| // resultado es Adulto                                                             |");
+        System.out.println("---------------------------------- ---------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+
+
+
+
+
 }
--- src/menus/SubMenus.java
+++ src/menus/SubMenus.java
@@ -21,6 +21,74 @@ public class SubMenus {
         System.out.println("-----------------------------------------------------");
         System.out.println("Selecciona una opcion");
     }
+    public static void stringSubMenu(){
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("                 ¿Que es un String?                                            ");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("| Es una clase que representa una secuencia de caracteres(texto)              |");
+        System.out.println("| A diferencia de los tipos de datos primitivos como int o double             |");
+        System.out.println("| que almacenan valores numéricos, String se utiliza para trabajar con texto. |");
+        System.out.println("| Sintaxis:                                                                   |");
+        System.out.println("| // Declaración e inicialización de un String                                |");
+        System.out.println("| String saludo = Hola, mundo!; el Hola Mundo va dentro de comillas           |");
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void constanteSubMenu(){
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("                 ¿Que es una Constante?                                          ");
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("| Son valores que no cambian durante la ejecución del programa. Se definen      |");
+        System.out.println("| usando la palabra clave final, lo que significa que una vez que se les asigna |");
+        System.out.println("| un valor, no se puede modificar.                                              |");
+        System.out.println("| Sintaxis:                                                                     |");
+        System.out.println("| public static final int MAX_USUARIOS = 100;                                   |");
+        System.out.println("| Se suelen escribir en Mayusculas y se separan las palaras con un guion bajo _ |");
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("Presiona Enter para continuar...");
+    }
+    public static void operadoresSubMenu(){
+        System.out.println("-----------------------------------------------------------------------------------");
+        System.out.println("               ¿Que son los Operadores y cuales son sus tipos?                     ");
+        System.out.println("-----------------------------------------------------------------------------------");
+        System.out.println("| los operadores son símbolos que realizan operaciones sobre valores y variables. |");
+        System.out.println("| los tipos de operadores mas comunes son:                                        |");
+        System.out.println("| 1. Operadores Aritmeticos                                                       |");
+        System.out.println("| 2. Operadores Relacionales                                                      |");
+        System.out.println("| 3. Operadores Logicos                                                           |");
+        System.out.println("| 4. Operadores Unarios                                                           |");
+        System.out.println("| 5. Operadores De Asignacion                                                     |");
+        System.out.println("| 6. Operadores Ternarios                                                         |");
+        System.out.println("-----------------------------------------------------------------------------------");
+        System.out.println("Selecciona una opcion para obtener una descripcion detallada:)");
+    }
+    public static void condicionalesMenu(){
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("               ¿En que consisten los Condicionales IF, ELSE IF, ELSE?                ");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("| Permiten ejecutar diferentes bloques de código si ciertas condiciones son         |");
+        System.out.println("| verdaderas o falsas. Aqui te explicamos en que consiste cada uno y su sintaxis    |");
+        System.out.println("| If: Esta declaración evalúa una condición. Si la condición es verdadera, se       |");
+        System.out.println("| ejecuta el bloque de codigo asociado. Sintaxis: if (condicion)                    |");
+        System.out.println("| » int x = 10; if (x > 5) { System.out.println( x es mayor que 5 ); }              |");
+        System.out.println("| Else if: Se usa después de una declaración if para evaluar una condicion          |");
+        System.out.println("| alternativa si la condicion de IF no es verdadera. Pueden haber multiples else if |");
+        System.out.println("| Sintaxis: if (condicion1){ se ejecuta si la condicion es verdadera}               |");
+        System.out.println("| else if(condicion2){ se ejecuta si la condicion es verdadera}                     |");
+        System.out.println("| » int x = 10; if (x > 15) { System.out.println( x es mayor que 15 ); }            |");
+        System.out.println("| else if (x > 5) {System.out.println( x es mayor que 5 pero menor o igual a 15 );} |");
+        System.out.println("| Else:  Se utiliza al final de una serie de condicionales para manejar el caso en  |");
+        System.out.println("| que ninguna de las condiciones anteriores (if o else if) sea verdadera.           |");
+        System.out.println("| Es opcional y captura todos los casos restantes. Sintaxis: if (condición1) {}     |");
+        System.out.println("| else if (condición2){} else {se ejecuta si ninguna de las anteriores es verdadera}|");
+        System.out.println("| »int x = 3; if (x > 5) {System.out.println( x es mayor que 5 );}                  |");
+        System.out.println("| else if (x == 5) {System.out.println( x es igual a 5 );}                          |");
+        System.out.println("| else {System.out.println( x es menor que 5 );}                                    |");
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Escribe 1 para mostrarte un ejemplo que aplica los condicionales IF, ELSE IF y ELSE");
+
+    }
+
 
 
 
