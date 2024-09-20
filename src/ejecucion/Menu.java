c6f97b11cd66d45b0b4ca6d50467ad86c3dcbd86
adrioviedo28 <adrioviedov@gmail.com>
2024-09-20 09:21:58 -0500

    ajuste en Menu principal

--- src/ejecucion/Menu.java
+++ src/ejecucion/Menu.java
@@ -39,24 +39,6 @@ public class Menu {
 
 
         do {
-            System.out.println("-----------------------------------------------------");
-            System.out.println("      Bienvenido al menú de Aleja y Adri jeje        ");
-            System.out.println("              LARREGO - AOVIED4                      ");
-            System.out.println("-----------------------------------------------------");
-            System.out.println("|      1. Datos Primitivos.                         |");
-            System.out.println("|      2. String.                                   |");
-            System.out.println("|      3. Constantes.                               |");
-            System.out.println("|      4. Tipo de Operadores.                       |");
-            System.out.println("|      5. Condicional IF, ELSE IF, ELSE.            |");
-            System.out.println("|      6. Condicional Switch.                       |");
-            System.out.println("|      7. Condicional Ternaria.                     |");
-            System.out.println("|      8. Bucle DO WHILE.                           |");
-            System.out.println("|      9. Bucle WHILE.                              |");
-            System.out.println("|     10. Bucle FOR.                                |");
-            System.out.println("|      0. Salir                                     |");
-            System.out.println("-----------------------------------------------------");
-
-            System.out.println("Selecciona una opción para explicarte uwu");
             MenuPrincipal.menuPrincipal();
             opciones = ingreseOpcion.nextInt();
 
